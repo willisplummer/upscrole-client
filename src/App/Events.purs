179fce3453f9db24bf722063c467cd7839568891
Willis Plummer <willisplummer@gmail.com>
2018-04-03 06:09:37 -0400

    post new messages to the socket

--- src/App/Events.purs
+++ src/App/Events.purs
@@ -1,22 +1,17 @@
 module App.Events where
 import App.Routes (Route)
 import App.State (State(..))
-import App.Types (Messages, Message)
+import App.Types (Messages)
 import Control.Applicative (pure)
-import Control.Apply ((*>))
-import Control.Monad.Aff.Console (log)
-import Control.Monad.Aff (Aff, makeAff)
-import Control.Monad.List.Trans (fromEffect)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE)
 import Control.SocketIO.Client (emit, Event, SocketIO) as Socket
-import Data.Array (snoc, head)
 import Data.Function (($))
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Maybe (Maybe(..))
 import Network.HTTP.Affjax (AJAX)
-import Prelude ((>>=), unit, bind, Unit)
 import Pux (EffModel, noEffects)
 import Pux.DOM.Events (DOMEvent, targetValue)
+import Prelude (bind)
 
 dataEvent :: Socket.Event
 dataEvent = "data"
@@ -32,10 +27,11 @@ foldp (MessageSubmit) (State st) =
   { state: State st { messageInput = "" }
   , effects: [
     case st.socket of
-      Just socket -> liftEff $ Socket.emit socket dataEvent st.messageInput
-      _ -> Noop
+      Just socket -> do
+        _ <- liftEff $ Socket.emit socket dataEvent { body: st.messageInput }
+        pure Nothing
+      Nothing -> (pure Nothing)
     ]
   }
-foldp (MessageSubmit) (State st) = noEffects $ State st
 foldp (OnMessage m) (State st) = noEffects $ State st {messages = m}
 foldp Noop st = noEffects $ st
